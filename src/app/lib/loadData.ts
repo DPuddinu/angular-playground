dab3ab1691bf1f0e57cf2b6d3836ca48b885a7c8
Dario Puddinu <dario.puddinu@abinsula.com>
2024-12-02 17:53:45 +0100

    chore: indentation

--- src/app/lib/loadData.ts
+++ src/app/lib/loadData.ts
@@ -2,9 +2,7 @@ import { Observable, of } from 'rxjs';
 import { catchError, map, startWith } from 'rxjs/operators';
 import { Errored, Loaded, Loading, LoadingState } from '../types/loading.type';
 
-export function toLoadingStateStream<T>(
-  source$: Observable<T>
-): Observable<LoadingState<T>> {
+export function toLoadingStateStream<T>( source$: Observable<T> ): Observable<LoadingState<T>> {
   return source$.pipe(
     map((data) => ({ state: 'loaded', data } as Loaded<T>)),
     catchError((error) => of({ state: 'error', error } as Errored)),
