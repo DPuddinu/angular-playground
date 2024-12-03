d2cb6a2fdfc9445110fb1f4f406319e84669c900
Dario Puddinu <dario.puddinu@abinsula.com>
2024-12-03 17:51:47 +0100

    edit app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  template: `<div>{{ title }}</div>`
 })
 export class AppComponent {
   title = 'angular-playground';
